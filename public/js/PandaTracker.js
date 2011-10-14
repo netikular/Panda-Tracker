ab63b0c234796c4ed48ec2382d672929354e4057
Kevin Pratt <kpratt@bwc.org>
2011-10-14 08:28:42 +0200

    Another round of look and feel for the backbone version

--- public/js/PandaTracker.js
+++ public/js/PandaTracker.js
@@ -45,10 +45,62 @@
   window.tasklist = new TaskList();
   task = new Task({'abstract':'stuff', 'description':'more stuff','selected':true});
   task2 = new Task({'abstract':'stuff2', 'description':'more stuff2','selected':false});
+  task3 = new Task({'abstract':'stuff3', 'description':'more stuff3','selected':false});
   window.tasklist.add(task);
   window.tasklist.add(task2);
-  
+  window.tasklist.add(task3);
+  //
   //Views
+  //
+  window.TaskListView = Backbone.View.extend({
+    tagName: "tbody",
+    className:"results-list",
+
+    initialize: function(){
+      _.bindAll(this,'render');
+    },
+    
+    render: function(){
+      this.collection.each(function(task){
+        var view = new TaskView({model:task});
+        $(this.el).append(view.render().el);
+      },this);
+
+      $(this.el).sortable({
+        helper:this.fixHelper,
+        start:this.start,
+        stop:this.stop,
+        placeholder: "ui-state-highlight"
+      }).disableSelection();
+
+      return this;
+    },
+
+    //Helper functions to make sure everything looks good while the sort is working
+    fixHelper: function(e,ui) {
+      ui.children().each(function() {
+        $(this).width($(this).width());
+      });
+      return ui;
+    },
+
+    start: function(e,ui){
+      $(ui.placeholder).height($(ui.item).height()).append("");
+      $(ui.item).addClass("ui-state-highlight");
+      $(ui.item).children().each(function(index,child) {
+        $(child).addClass("no-border");
+      });
+    },
+
+    stop: function(e,ui){
+      $(ui.item).removeClass("ui-state-highlight");
+      $(ui.item).children().each(function(index,child) {
+        $(child).removeClass("no-border");
+      });
+    }
+    //end of the helpers
+  });
+
   window.TaskView = Backbone.View.extend({
     template: "#task-template",
     tagName: 'tr',
@@ -68,51 +120,104 @@
     }
   });
 
+  window.SelectButton = Backbone.View.extend({
+      template: "#select-button-template",
+      className: "",
+      tagName: "span",
+
+      events: {
+        "click .select-all": "showDropdown",
+        "click input": "selectAll",
+        "click .dropdown li": "doAction"
+      },
+
+      showRemoveme: function(){
+        $(".removeme").remove();
+        $("body").append('<div class="removeme">&nbsp;</div>');
+        var self = this;
+        $(".removeme").one("click",function(){
+          self.hideDropdown();
+        });
+      },
+
+      selectAll: function(e){
+        console.log("select all");
+        e.stopPropagation()
+      },
+
+      doAction : function(){
+        this.hideDropdown();
+        console.log("do-work2");
+      },
+      
+      showDropdown: function(){
+        $(this.el).find(".dropdown").show();
+        this.showRemoveme();
+      },
+
+      hideDropdown: function(){
+        $(this.el).find(".dropdown").hide();
+        $(".removeme").remove();
+      },
+
+      initialize: function(){
+        _.bindAll(this,'render');
+        this.template = _.template($(this.template).html());
+      },
+      
+      render: function(){
+        $(this.el).html(this.template());   
+        this.$('.select-all').button({
+            text:true,
+            icons : {
+              secondary: 'ui-icon-triangle-1-s'
+              }
+        });
+
+        return this;
+      }
+  });
+
   window.ToolBar = Backbone.View.extend({
-      template: "#toolbar-template",
-      className: "ui-widget-header ui-corner-all toolbar",
+    template: "#toolbar-template",
+    className: "ui-widget-header ui-corner-all toolbar",
+
+    events: {
+      "click .close": "close",
+      "click .remove": "remove",
+      "click .send": "send",
+      "click .settings": "settings"
+    },
+
+    send: function(){
+      console.log("send");
+    },
+
+    remove: function(){
+      console.log("remove");
+    },
+
+    close: function(){
+      console.log("close");
+    },
+
+    settings: function(){
+      console.log("settings");
+    },
 
     initialize: function(){
       _.bindAll(this,'render');
       this.template = _.template($(this.template).html());
+      $(this.el).addClass(this.className);
     },
     
     render: function(){
       $(this.el).html(this.template());   
 
-      this.$('.select-all').button({
-          text:true,
-          icons : {
-            secondary: 'ui-icon-triangle-1-s'
-            }
-        })
-        .find("input")
-        .click(function(e){e.stopPropagation()});
+      var sc = new SelectButton();
+      $(this.el).find(".select-container").html(sc.render().el);
 
-      this.$('.select-container').find(".dropdown li").click(function(){console.log("do-work2");});
-
-      this.$(".send").button()
-        .next()
-          .button( {
-              text: false,
-              icons: {
-                  primary: "ui-icon-triangle-1-s"
-              }
-              })      .parent().find(".dropdown li").click(function(){console.log("do-work");});
-
-
-      this.$(".tag-commands").buttonset();
-
-      this.$(".actions-select, .select-all,.tag-select").click(function(){
-        var $parent = $(this).parent();
-        $parent.find(".dropdown").toggle();
-        $("body").append('<div class="removeme">&nbsp;</div>');
-        $(".removeme").click(function(){
-          $parent.find('.dropdown').hide();
-          $(this).remove();
-        });
-      }).parent().find(".dropdown li").click(function(){$(this).parent().toggle();$(".removeme").remove();});
-
+      this.$(".send").button();
       this.$(".close").button();
       this.$(".remove").button();
       this.$(".common-commands").buttonset();
