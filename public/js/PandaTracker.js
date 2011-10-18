2cd42c8249b960ef6397959c9a3fd9470491815a
Kevin Pratt <kpratt@bwc.org>
2011-10-18 11:12:08 +0200

    Finished the preliminary form and got the TaskFormView to pop at the right moments.

--- public/js/PandaTracker.js
+++ public/js/PandaTracker.js
@@ -1,6 +1,56 @@
+/*!
+ * jQuery serializeObject - v0.2 - 1/20/2010
+ * http://benalman.com/projects/jquery-misc-plugins/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+
+// Whereas .serializeArray() serializes a form into an array, .serializeObject()
+// serializes a form into an (arguably more useful) object.
+
+(function($,undefined){
+  '$:nomunge'; // Used by YUI compressor.
+  
+  $.fn.serializeObject = function(){
+    var obj = {};
+    
+    $.each( this.serializeArray(), function(i,o){
+      var n = o.name,
+        v = o.value;
+        
+        obj[n] = obj[n] === undefined ? v
+          : $.isArray( obj[n] ) ? obj[n].concat( v )
+          : [ obj[n], v ];
+    });
+    
+    return obj;
+  };
+  
+})(jQuery);
+
 (function($) {
   //Models
   window.Task = Backbone.Model.extend({
+    //I don't like that i have to set the defaults.
+    // Maybe this will go away once the object is initialized
+    // from the server
+    defaults: {
+                "selected": false,
+                "description": "",
+                "status": "",
+                "action_date": "",
+                "priority": "",
+                "due_date": "",
+                "initials_needed": "",
+                "aide": "",
+                "assigned": "",
+                "confidential": "",
+                "abstract": "",
+                "wizard_numbers": "",
+                "cap_numbers": ""
+    },
     
     isClosed: function() {
       return this.get('closed');
@@ -58,11 +108,14 @@
 
     initialize: function(){
       _.bindAll(this,'render');
+      window.tasklist.bind("add",this.render,this);
+      this.taskform = this.options["taskform"];
     },
     
     render: function(){
+      $(this.el).empty();
       this.collection.each(function(task){
-        var view = new TaskView({model:task});
+        var view = new TaskView({model:task,taskform:this.taskform});
         $(this.el).append(view.render().el);
       },this);
 
@@ -112,8 +165,7 @@
     },
 
     edit: function(e) {
-      var tf = new TaskFormView({model: this.model});
-      tf.show();
+      this.taskform.show(this.model);
     },
 
     select: function(e) {
@@ -132,9 +184,10 @@
     },
 
     initialize: function(){
-      _.bindAll(this,'render','highlight');
+      _.bindAll(this,'render','highlight','edit');
       this.template = _.template($(this.template).html());
-      this.model.bind('change:selected',this.highlight);
+      this.model.bind('change',this.render);
+      this.taskform = this.options["taskform"];
     },
     
     render: function(){
@@ -148,12 +201,29 @@
     template: "#task-form-template",
     el: "#task-form",
 
+    events: {
+      "submit form": "submit"
+    },
+
+    submit: function(e) {
+
+      this.model.set($(this.el).find("form").serializeObject());
+
+      if (!this.collection.include(this.model))
+        this.collection.add(this.model);
+
+      $(this.el).dialog("close");
+      e.preventDefault();
+    },
+
     initialize: function() {
       _.bindAll(this,'render');
       this.template = _.template($(this.template).html()); 
+      this.collection = this.options["collection"];
     },
 
     render: function() {
+      $(this.el).empty();
       $(this.el).html(this.template(this.model.toJSON()));
       $(this.el).dialog({
         autoOpen: false,
@@ -165,12 +235,16 @@
       return this;
     },
 
-    show: function() {
+    show: function(model) {
       //is this the best way? -- satisfies the need to clear the form each time.
+      this.model = model;
       this.render(); 
       $(this.el).dialog("open");
     },
+
     close: function() {
+      console.log("close");
+      $(this.el).empty();
       //handle some stuff here cause closing is awesome
     },
   });
@@ -246,8 +320,7 @@
     },
 
     new: function() {
-      var new_task = new TaskFormView({model: new Task()});
-      new_task.show();
+      this.taskform.show(new Task());
     },
 
     send: function(){
@@ -270,7 +343,7 @@
       _.bindAll(this,'render');
       this.template = _.template($(this.template).html());
       $(this.el).addClass(this.className);
-      this.dialog = this.options["dialog"];
+      this.taskform = this.options["taskform"];
     },
     
     render: function(){
